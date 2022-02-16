94c098aa060d84c1d9950164872e238477168744
nicspalla <nic.spalla@gmail.com>
2022-02-16 11:22:21 +0100

    solved issue related to fortran preprocess with intel compilers

--- packages/yambo/package.py
+++ packages/yambo/package.py
@@ -2,8 +2,8 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
 import os
+
 from spack import *
 
 
@@ -41,7 +41,6 @@ class Yambo(AutotoolsPackage, CudaPackage):
     version('4.5.0', sha256='c68b2c79acc31b3d48e7edb46e4049c1108d60feee80bf4fcdc4afe4b12b6928')
     version('4.4.1', sha256='2daf80f394a861301a9bbad559aaf58de283ce60395c9875e9f7d7fa57fcf16d')
     version('4.3.3', sha256='790fa1147044c7f33f0e8d336ccb48089b48d5b894c956779f543e0c7e77de19')
-    # version('4.2.5', sha256='9df63af4c6445434f45b124e0e9d3a20a090d1f0f821bb55fd51ab5a4dccf5ac')
 
     patch('hdf5.patch', sha256='b9362020b0a29abec535afd7d782b8bb643678fe9215815ca8dc9e4941cb169f', when='@4.3:')
     patch('hdf5_old.patch', sha256='c1ce59d15ab30b33fa474fa58404344254c4203d1762316e178a19cc3a9993b6', when='@:4.2.99')
@@ -59,8 +58,6 @@ class Yambo(AutotoolsPackage, CudaPackage):
 "parallel" uses SCALAPACK and "slepc" is used for diagonalization of BSE""")
     conflicts('linalg=parallel', when='~mpi',
               msg="Parallel linear algebra available only with +mpi")
-    conflicts('linalg=slepc', when='@:4.2.2',
-              msg="SLEPc support for linear algebra available only from yambo@4.3.3")
     depends_on('blas')
     depends_on('lapack')
     depends_on('netlib-lapack%nvhpc', when='%nvhpc')
@@ -80,6 +77,8 @@ class Yambo(AutotoolsPackage, CudaPackage):
     conflicts('%gcc', when='+cuda',
               msg="CUDA-Fortran available only with NV or PGI compilers")
     conflicts('%intel', when='+cuda',
+              msg="CUDA-Fortran available only with NV or PGI compilers")
+    conflicts('%oneapi', when='+cuda',
               msg="CUDA-Fortran available only with NV or PGI compilers")
 
     # Other variants
@@ -120,10 +119,10 @@ class Yambo(AutotoolsPackage, CudaPackage):
         filter_file('config/report', report_abspath, 'configure')
 
     def enable_or_disable_time(self, activated):
-        return '--enable-time-profile' if activated else '--disable-time-profile'  # noqa: E501
+        return '--enable-time-profile' if activated else '--disable-time-profile'
 
     def enable_or_disable_memory(self, activated):
-        return '--enable-memory-profile' if activated else '--disable-memory-profile'  # noqa: E501
+        return '--enable-memory-profile' if activated else '--disable-memory-profile'
 
     def enable_or_disable_openmp(self, activated):
         return '--enable-open-mp' if activated else '--disable-open-mp'
@@ -154,14 +153,15 @@ class Yambo(AutotoolsPackage, CudaPackage):
             env.set('CC', "nvc")
             env.set('CPP', "cpp -E")
             env.set('F90SUFFIX', ".f90")
+        if '%intel' in spec:
+            env.set('FPP', "ifort -E -free -P")
         if 'mkl' in spec:
-            try:
-                mklroot = os.environ['MKLROOT']
-            except KeyError:
-                env.set('MKLROOT', '{}/mkl/latest/lib/intel64'.format(spec['blas'].prefix))
+            if 'MKLROOT' not in os.environ:
+                env.set('MKLROOT', '{}/mkl/latest'.format(spec['blas'].prefix))
 
     def configure_args(self):
         spec = self.spec
+        print(os.environ['PATH'])
 
         args = [
             '--enable-msgs-comps',
@@ -206,8 +206,10 @@ class Yambo(AutotoolsPackage, CudaPackage):
             args.append('--enable-par-linalg')
             if 'mkl' in spec and 'intel' in spec['mpi'].name:
                 args.extend([
-                    '--with-blacs-libs=-L{0}/lib/intel64 -lmkl_blacs_intelmpi_lp64'.format(env['MKLROOT']),
-                    '--with-scalapack-libs=-L{0}/lib/intel64 -lmkl_scalapack_lp64'.format(env['MKLROOT']),
+                    '--with-blacs-libs=-L{0}/lib/intel64 '
+                    '-lmkl_blacs_intelmpi_lp64'.format(env['MKLROOT']),
+                    '--with-scalapack-libs=-L{0}/lib/intel64 '
+                    '-lmkl_scalapack_lp64'.format(env['MKLROOT']),
                 ])
             else:
                 args.extend([
@@ -220,7 +222,6 @@ class Yambo(AutotoolsPackage, CudaPackage):
                 '--with-petsc-path={0}'.format(spec['petsc'].prefix),
                 '--with-slepc-path={0}'.format(spec['slepc'].prefix),
             ])
-
 
         # I/O
         args.extend([
